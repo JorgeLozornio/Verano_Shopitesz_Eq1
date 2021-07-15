7b595b3063923cdf84fde4934ed61504ace5d666
JorgeLozornio <jlozornio321@accitesz.com>
2021-07-14 22:44:09 -0500

    Layout update

--- Aplicacion/app.py
+++ Aplicacion/app.py
@@ -24,5 +24,37 @@ def perfil():
 def productos():
     return render_template('productos/productos_general.html')
 
+
+#_______________RUTAS RELACIONADAS CON LOS PEDIDOS_______________#
+#REDIRECCIONA A EL PEDIDO DEL CLIENTE
+@app.route('/usuarios/pedido')
+def usuarioPedido():
+    return render_template('pedidos/compra.html')
+
+#REDIRECCIONA A LAS COMPRAS HECHAS POR EL USUARIO
+@app.route('/usuarios/compras')
+def usuarioCompras():
+    return render_template('pedidos/pedidos.html')
+
+#REDIRECCIONA A LA PAGINA DE SEGUIMIENTO DEL PEDIDO
+@app.route('/usuarios/pedidos/seguimiento')
+def usuarioSegPedido():
+    return render_template('pedidos/seguimiento.html')
+
+
+#_______________RUTAS RELACIONADAS CON LA CESTA_______________#
+#REDIRECCIONA A LA CESTA DEL CLIENTE
+@app.route('/usuarios/cesta')
+def cesta():
+    return render_template('cesta/cesta.html')
+
+
+#_______________RUTAS RELACIONADAS CON LAS TARJETAS_______________#
+#REDIRECCIONA A LA PAGINA PARA AGREGAR TARJETAS
+@app.route('/usuarios/tarjetas')
+def tarjetas():
+    return render_template('tarjetas/Altatarjetas.html')
+
+
 if __name__=='__main__':
     app.run(debug=True)
